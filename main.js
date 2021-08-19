227e57ba36640809fc57a71aa17149b0f861308a
YafitSamuel <gyafit642@gmail.com>
2021-08-19 22:14:24 +0300

    commit

---
+++ main.js
@@ -0,0 +1,204 @@
+
+
+//    ==================================19.08.2021========================================
+
+// function printCar(color, company, smk) {
+//     var car = {};
+//     car.color = color;
+//     car.company = company;
+//     car.smk = smk;
+//     car.year = new Date().getFullYear();
+//     document.getElementById("div1").innerText = car.color
+//     document.getElementById("div2").innerText = car.company
+//     document.getElementById("div3").innerText = car.smk
+//     document.getElementById("div4").innerText= car.year
+// };
+// printCar(prompt("enter a color"), prompt("enter a company"), prompt("enter a smk"));
+// ================1==================
+
+// function fullNameAndAge(age, name) {
+//     var person = {}
+//     person.age=age
+//     person.name=name
+//     console.log(person);
+// }
+//     fullNameAndAge(Number(prompt ("ente age")),prompt("enter name"))
+// ==================7=================
+// function fullNameAndAge(age, name) {
+//     document.write(<div>`name:${name},age:${age}`</div>)
+// }
+// fullNameAndAge(prompt("enter a age"), prompt("enter your name"));
+// ==================8=================
+// function printmynag(firstname, lastname, age){
+// document.write(` <div> firstname:${firstname},lastname:${lastname}, age:${age} </div>`)
+// }
+// printmynag(prompt("enter your firstname"), prompt("enter your age"), prompt("enter your lastname"));
+
+// דוגמאות בכיתה:
+// ==================9=================
+// var divArray = document.getElementsByClassName("myDiv")
+// for (var i = 0; i < divArray.length; i++) {
+//     divArray[i].style = `width:${Math.floor(Math.random() * 1000)}px ;height:${Math.floor(Math.random() * 100)}px`
+// }
+
+//  var divArray=document.getElementsByTagName("div")
+//  for(var i=0;i<divArray.length;i++){
+//  divArray[i].innerHTML=i
+//  divArray[i].style.'color'=i
+//  }
+
+
+
+// ==========================משימת סופש-19.08.2021========================================
+// ==================1=================
+// function printHelloThere() {
+//         document.write("hello there")
+// }
+// printHelloThere()
+// ==================2=================
+// function printHelloThereDavid() {
+//     document.write("hello david")
+// }
+// printHelloThereDavid()
+// ==================3=================
+
+// function printHelloThere() {
+//     document.write(`hello ther ${prompt("enter name")}`)
+// }
+// printHelloThere()
+// ==================4=================
+
+// function printmynag(firstname, lastname, age){
+// document.write(` <div> firstname:${firstname},lastname:${lastname}, age:${age} </div>`)
+// }
+// printmynag(prompt("enter your firstname"), prompt("enter your age"), prompt("enter your lastname"));
+
+// ==================5=================
+// 
+
+
+
+
+// ==================6=================
+
+
+
+
+
+// ==================7=================
+// function fullNameAndAge(age, name) {
+//     document.write(`<div>name:${name},age:${age}</div>`)
+// }
+// fullNameAndAge(prompt("enter a age"), prompt("enter your name"));
+
+// ==================8=================
+
+// function fullNameAndAge(age, name) {
+//     document.write(`<div><p>name:${name},age:${age}</p></div>`)
+// }
+// fullNameAndAge(prompt("enter a age"), prompt("enter your name"));
+
+// ==================9=================
+
+// function fullNameAndAge(age, name) {
+//     document.write(`<p id="li">name:${name},age:${age}</p>`)
+// }
+// fullNameAndAge(prompt("enter a age"), prompt("enter your name"));
+
+// ==================10=================
+// function fullNameAndAge(age, name) {
+//     document.write(`<div><p id="hi">name:${name},age:${age}</p></div>`)
+// }
+// fullNameAndAge(prompt("enter a age"), prompt("enter your name"));
+// ==================11=================
+// function myName(name) {
+//     document.write(`<p id="as">name:${name}</p>`)
+// }
+// myName(prompt("enter your name"));
+
+// function printlastName(lastName) {
+//     document.write(document.getElementById("as").innerText + ` lastName:${lastName}`)
+//     console.log(document.getElementById("as").innerText + ` lastName:${lastName}`)
+// }
+// printlastName("samuel");
+
+// ==================12=================
+// function myName(name) {
+//     document.write(`<p id="as">name:${name}</p>`)
+// }
+// myName(prompt("enter your name"));
+
+// function printlastName(lastName) {
+//     document.write(document.getElementById("as").innerText + ` lastName:${lastName}` +  new Date().getFullYear())
+//     console.log(document.getElementById("as").innerText + ` lastName:${lastName}` + new Date().getFullYear())
+// }
+// printlastName("samuel");
+
+// ==================13=================
+
+// function printCar(color, company, smk) {
+//     var car = {};
+//     car.color = color;
+//     car.company = company;
+//     car.smk = smk;
+
+//     document.write(`${car.color},${car.company},${car.smk}`); 
+//     console.log(car);     
+// };
+// printCar(prompt("enter a color"), prompt("enter a company"), prompt("enter a smk"));
+
+// ==================14=================
+
+// function printCar(color, company, smk) {
+//     var car = {};
+//     car.color = color;
+//     car.company = company;
+//     car.smk = smk;
+//     car.year = new Date().getFullYear();
+
+//     document.write(`${car.color},${car.company},${car.smk},${car.year}`);
+//     console.log(car);
+
+// };
+// printCar(prompt("enter a color"), prompt("enter a company"), prompt("enter a smk"));
+
+// ==================15=================
+// function printCar(color, company, smk) {
+//     var car = {};
+//     car.color = color;
+//     car.company = company;
+//     car.smk = smk;
+
+//     document.write(`<div>${car.color},${car.company},${car.smk}</div>`); 
+//     console.log(car);     
+// };
+// printCar(prompt("enter a color"), prompt("enter a company"), prompt("enter a smk"));
+// ==================16=================
+// function printCar(color, company, smk) {
+//         var car = {};
+//         car.color = color;
+//         car.company = company;
+//         car.smk = smk;
+    
+//         document.write(`<div>${car.color}</div><p>${car.company}</p><h1>${car.smk}</h1>`); 
+//         console.log(car);     
+//     };
+//     printCar(prompt("enter a color"), prompt("enter a company"), prompt("enter a smk"));
+
+// ==================17=================
+// function printCar(color, company, smk) {
+//     var car = {};
+//     car.color = color;
+//     car.company = company;
+//     car.smk = smk;
+//     car.year = new Date().getFullYear();
+//     document.getElementById("div1").innerText = car.color
+//     document.getElementById("div2").innerText = car.company
+//     document.getElementById("div3").innerText = car.smk
+//     document.getElementById("div4").innerText= car.year
+// };
+// printCar(prompt("enter a color"), prompt("enter a company"), prompt("enter a smk"));
+
+
+
+
